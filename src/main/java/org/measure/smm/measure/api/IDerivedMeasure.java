bee08dd4b84e322b12aa2a649bd2d7e5a71fd286
Antonin Abherve <antonin.abherve@softeam.fr>
2017-02-15 17:39:11 +0100

    extend Measurement api

--- src/main/java/org/measure/smm/measure/api/IDerivedMeasure.java
+++ src/main/java/org/measure/smm/measure/api/IDerivedMeasure.java
@@ -3,6 +3,7 @@ package org.measure.smm.measure.api;
 import java.util.List;
 
 public interface IDerivedMeasure extends IMeasure {
+	public void cleanMeasureInput();
 	public void addMeasureInput(String reference,String role, IMeasurement value);
 	public List<IMeasurement> calculateMeasurement() throws Exception;
 }
--- src/main/java/org/measure/smm/measure/defaultimpl/measures/DerivedMeasure.java
+++ src/main/java/org/measure/smm/measure/defaultimpl/measures/DerivedMeasure.java
@@ -11,49 +11,54 @@ import org.measure.smm.measure.api.IMeasurement;
 public abstract class DerivedMeasure implements IDerivedMeasure {
 
 	protected Map<String, String> properties = new HashMap<>();;
-	protected Map<String,List<IMeasurement>> referenceMap = new HashMap<>();
-	protected Map<String,List<IMeasurement>> roleMap = new HashMap<>();;
+	protected Map<String, List<IMeasurement>> referenceMap = new HashMap<>();
+	protected Map<String, List<IMeasurement>> roleMap = new HashMap<>();;
 
 	@Override
-	public Map<String,String> getProperties(){
+	public Map<String, String> getProperties() {
 		return this.properties;
 	}
-		
-	public String getProperty(String property){
-		String result =  properties.get(property);		
-		if(result != null){
+
+	public String getProperty(String property) {
+		String result = properties.get(property);
+		if (result != null) {
 			return result;
-		}		
+		}
 		return "";
 	}
 
-	public List<IMeasurement> getMeasureInputByName(String name){
+	public List<IMeasurement> getMeasureInputByName(String name) {
 		return referenceMap.get(name);
 	}
-	
-	public List<IMeasurement> getMeasureInputByRole(String role){
+
+	public List<IMeasurement> getMeasureInputByRole(String role) {
 		return roleMap.get(role);
 	}
-	
-	
+
 	@Override
-	public void addMeasureInput(String reference,String role, IMeasurement value){
+	public void addMeasureInput(String reference, String role, IMeasurement value) {
 		List<IMeasurement> references = referenceMap.get(reference);
-		if(references == null){
+		if (references == null) {
 			references = new ArrayList<>();
-			referenceMap.put(reference,references);
+			referenceMap.put(reference, references);
 		}
 		references.add(value);
-		
+
 		List<IMeasurement> roles = roleMap.get(role);
-		if(roles == null){
+		if (roles == null) {
 			roles = new ArrayList<>();
-			roleMap.put(role,roles);
+			roleMap.put(role, roles);
 		}
-		roles.add(value);	
+		roles.add(value);
+	}
+
+	@Override
+	public void cleanMeasureInput() {
+		this.referenceMap = new HashMap<>();
+		this.roleMap = new HashMap<>();
 	}
 
 	@Override
 	public abstract List<IMeasurement> calculateMeasurement() throws Exception;
-	
+
 }
